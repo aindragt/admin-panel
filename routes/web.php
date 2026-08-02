6f8076e187b5b17bd23845fae9b0b55cc667ea41
Indra Pramudya <aindragt@gmail.com>
2026-08-02 17:32:49 +0700

    feat(data-table): implement Issue #5 - Data Table CRUD view page

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('pages.dashboard');})->name('dashboard');
 Route::get('/forms', fn() => view('pages.forms'))->name('forms.demo');
 Route::get('/users', fn() => view('pages.users'))->name('users.index');
-Route::get('/login', fn() => view('auth.login'))->name('login');
