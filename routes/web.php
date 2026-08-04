61d659c9bdd4eaa19848d157976b6dd1931800ca
Indra Pramudya <aindragt@gmail.com>
2026-08-04 11:49:10 +0700

    hapus route /users wok, salah pengalamatan soalnya.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('pages.dashboard');})->name('dashboard');
 Route::get('/forms', fn() => view('pages.forms'))->name('forms.demo');
-Route::get('/users', fn() => view('pages.users'))->name('users.index');
