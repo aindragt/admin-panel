91d098c5463dca54fbc76a4595c139cb07facd50
Indra Pramudya <aindragt@gmail.com>
2026-08-09 00:22:43 +0700

    feat(roles): add routes for role management

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('pages.dashboard');})->name('dashboard');
 Route::get('/forms', fn() => view('pages.forms'))->name('forms.demo');
 Route::get('/tables', fn() => view('pages.tables'))->name('tables.demo');
+Route::get('/roles', fn() => view('roles.index'))->name('roles.index');
+Route::get('/roles/create', fn() => view('roles.form'))->name('roles.create');
+Route::get('/roles/{id}/edit', fn() => view('roles.form'))->name('roles.edit');
 Route::get('/login', fn() => view('auth.login'))->name('login');
